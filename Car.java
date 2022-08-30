71f813ed5911a576c1a7369acca9b54e8e373d1f
OmuraisuSama <63500501+OmuraisuSama@users.noreply.github.com>
2022-08-30 11:15:56 +1000

    Add Car class

---
+++ Car.java
@@ -0,0 +1,53 @@
+public class Car {
+    private String fName;
+    private String lName;
+    private boolean isStaff;
+    private String regoNumber;
+
+    // ------------ CONSTRUCTORS ---------------
+    public Car(String regoNumber, String fName, String lName, boolean isStaff) {
+        this.regoNumber = regoNumber;
+        this.fName = fName;
+        this.lName = lName;
+        this.isStaff = isStaff;
+    }
+
+    // ----------- END CONSTRUCTORS ------------
+
+
+    // --------------- GETTERS -----------------
+    // method to get owner names
+    public String [] getNames() {
+        return {fName, lName};
+    }
+
+    // method to find if the car is a staff car
+    public boolean isStaff() {
+        return isStaff;
+    }
+
+    // method to get rego number
+    public String getRegoNumber() {
+        return regoNumber;
+    }
+    // ------------- END GETTERS ---------------
+
+
+    // --------------- SETTERS -----------------
+    // method to set rego number
+    public void setRegoNumber(String regoNumber) {
+        this.regoNumber = regoNumber;
+    }
+
+    // method to set owner names
+    public void setNames(String fName, String lName) {
+        this.fName = fName;
+        this.lName = lName;
+    }
+
+    // method to set whether the car is a staff car
+    public void setForStaff(boolean forStaff) {
+        this.forStaff = forStaff;
+    }
+    // ------------- END SETTERS ---------------
+}
